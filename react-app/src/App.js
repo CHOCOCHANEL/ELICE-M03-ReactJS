fc8e438f257d7e3a8f13c46ebc07e4d659fdfc74
[SW2/서울/1반] 정재홍 <jh780465@gmail.com>
2022-06-06 14:44:13 +0900

    My First React App Completed Successfully.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,26 +4,40 @@ import './App.css';
 function Header(){
   return <header><h1><a href="/">WWW</a></h1></header>
 }
-function Nav(){
+
+function Nav(props){
+  const topics = props.data;
+  let list = topics.map(topic => <li key={topic.id}><a href={'/read/'+topic.id}>{topic.title}</a></li>);
+
   return <nav>
     <ol>
-        <li><a href="/read/1">html</a></li>
-        <li><a href="/read/2">css</a></li>
+        {list}
     </ol>
   </nav>
 }
-function Article(){
+
+function Article(props){
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB!
+    <h2>{props.title}</h2>
+    <p>{props.body}</p>
   </article>
 }
+
 function App() {
+  const topics = [
+    {id: 1, title: 'html', body: 'html is ...'},
+    {id: 2, title: 'css', body: 'css is ...'},
+    {id: 3, title: 'javascript', body: 'javascript is ...'},
+  ];
+
   return (
     <div>
       <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Nav data={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB!"></Article>
+
+      <img src="a.png"></img>
+      <a href="http://info.cern.ch">web</a>
     </div>
   );
 }
